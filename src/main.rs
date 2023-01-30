70f6e351f550dec5de61414ad4f71d94b9d0dff2
MateusMamani <matmanymat@gmail.com>
2023-01-29 21:51:10 -0300

    feat(render-colors): screen with deferente colors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 //extern crate sdl2;
-
+//https://sunjay.dev/learn-game-dev/refactor-traditional-game-loop.html
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::WindowCanvas;
-use std::time::Duration;
 
 fn render(canvas: &mut WindowCanvas, color: Color) {
     canvas.set_draw_color(color);
@@ -12,38 +11,40 @@ fn render(canvas: &mut WindowCanvas, color: Color) {
     canvas.present();
 }
 
-fn main() { 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn main() -> Result<(), String>{ 
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem.window("Game Tutorial", 800, 600)
         .position_centered()
         .build()
-        .unwrap();
+        .expect("Error no video");
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Canvas falhou");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
+        //Handle events
+        for event in event_pump.poll_iter(){
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    break 'running;
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+        //Update
+        i = (i + 1) % 255;
 
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        println!("{}", i);
+        println!("Hello, world!")
     }
+    Ok(())
 }
