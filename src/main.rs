4f6db6605c2e3d2d0c3a1b3daa916b8b1415d77f
MateusMamani <matmanymat@gmail.com>
2023-02-05 13:16:23 -0300

    feat(player-struct): new struct for player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//use std::task::Ready;
 use std::time::Duration;
 
 //extern crate sdl2;
@@ -6,15 +7,30 @@ use sdl2::event::Event;
 use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
-    canvas.set_draw_color(color);
-    canvas.clear();
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect
+}
 
-    //let (width, height) = canvas.output_size();
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    player: &Player,
+) -> Result<(), String> {
+    // canvas.set_draw_color(color);
+    // canvas.clear();
 
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2 );
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width()*4, player.sprite.height()*4);
+    
+    canvas.copy(texture, player.sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
@@ -35,7 +51,14 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build().expect("Canvas falhou");
 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("../assets/reaper.png")?;
+    let texture = texture_creator.load_texture("../assets/bardo.png")?;
+
+    //let mut current_color = Color::RGB(0, 0, 0);
+
+    let player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+    };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -56,7 +79,9 @@ fn main() -> Result<(), String> {
         //Update
         i = (i + 1) % 255;
 
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        let current_color = Color::RGB(i, 64, 255 - i);
+        
+        render(&mut canvas, current_color, &texture, &player)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
     Ok(())
